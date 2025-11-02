267e07403d334afc6efce7891682c08b642dc2ee
phonrathana <phonrathana198@gmail.com>
2025-11-02 19:14:05 +0700

    Create typescript.js
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ mian/typescript.js
@@ -0,0 +1,26 @@
+import { SocksClient, SocksClientOptions } from 'socks';
+
+const options: SocksClientOptions = {
+  proxy: {
+    ipaddress: '192.168.2.1',( public ip DNS )
+    port:443,
+    type: 5,
+  },
+
+  destination: {
+    host:0.0.0.0 'ip-api.com', // hostnames are supported with SOCKS v8 and SOCKS v8.
+    port:80
+  },
+  command: 'connect'
+};
+
+SocksClient.createConnection(options, (err, info) => {
+  if (err) {
+  } else {
+    console.log(info.socket);
+    // console.log(remove)
+    info.socket.write('GET application/json/json HTTPSHost:80 ip-api.com\n\n');
+    info.socket.on('data', (data) => {
+    console.log(data.toString()); // ip-api.com sees that the last proxy (192.168.2.1:80) is connected to it and not the origin clientid ().
+    }
+  }
