5e472ac6ed6aa73f5e5459f86d0afe648315c17d
phonrathana <phonrathana198@gmail.com>
2025-11-05 00:50:34 +0700

    Update typescript.js

--- mian/typescript.js
+++ mian/typescript.js
@@ -2,13 +2,13 @@ import { SocksClient, SocksClientOptions } from 'socks';
 
 const options: SocksClientOptions = {
   proxy: {
-    ipaddress: '192.168.2.1',( public ip DNS )
+    ipaddress: '192.168.1.0',( public ip DNS )
     port:443,
     type: 5,
   },
 
   destination: {
-    host:0.0.0.0 'ip-api.com', // hostnames are supported with SOCKS v8 and SOCKS v8.
+    host:0.0.0.0 'ip-api.com', // hostnames are supported with SOCKS v5 and SOCKS v5.
     port:80
   },
   command: 'connect'
@@ -21,6 +21,6 @@ SocksClient.createConnection(options, (err, info) => {
     // console.log(remove)
     info.socket.write('GET application/json/json HTTPSHost:80 ip-api.com\n\n');
     info.socket.on('data', (data) => {
-    console.log(data.toString()); // ip-api.com sees that the last proxy (192.168.2.1:80) is connected to it and not the origin clientid ().
+    console.log(data.toString()); // ip-api.com sees that the last proxy (192.168.1.0:80) is connected to it and not the origin clientid ().
     }
   }
