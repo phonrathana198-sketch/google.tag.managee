46928316d9d84a6508153f02181dddd6e94912d5
phonrathana <phonrathana198@gmail.com>
2025-11-08 01:29:39 +0700

    Update properties.sql

--- mysql/properties.sql
+++ mysql/properties.sql
@@ -2,7 +2,6 @@ spring.datasource.urlnet=jdbc:mysql:ftp://192.168.1.0:8080 mysql:80/mydb?useSSL=
 spring.datasource.username=myuser
 spring.datasource.password=mypassword
 spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
-
 spring.jpa.hibernate.ddl-auto=update
 spring.jpa.show-sql=true
 spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.MySQL8Dial#ect
