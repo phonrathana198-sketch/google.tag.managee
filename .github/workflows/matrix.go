968786edd6fb0c08aac5f21649c9893dc9b6d5f8
phonrathana <phonrathana198@gmail.com>
2025-11-17 01:17:53 +0700

    Create matrix.go
    Signed-off-by: phonrathana <phonrathana198@gmail.com>

---
+++ .github/workflows/matrix.go
@@ -0,0 +1,37 @@
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        go: [ '1.14', '1.13' ]
+    name: Go ${{ matrix.go }} sample
+    steps:
+      - uses: actions/checkout@v5
+      - name: Setup go
+        uses: actions/setup-go@v6
+        with:
+          go-version: ${{ matrix.go }}
+      - run: go run hello.go
+          uses: actions/setup-go@v6
+  with:
+       token: ${{ secrets.GH_DOTCOM_TOKEN }}
+          go-version: '1.18'
+  steps:
+       - uses: actions/checkout@v5
+       - uses: actions/setup-go@v6
+    with:
+         go-version-file: '.tool-versions'
+       - run: go version
+   steps:
+      - uses: actions/checkout@v5
+      - uses: actions/setup-go@v6
+   with:
+      - run: go run hello.go
+        go-version: '1.17'
+        check-latest: true
+        cache-dependency-path: |
+             subdir/go.sum
+             tools/go.sum
+      # cache-dependency-path: "**/*.sum"
+   
+       
